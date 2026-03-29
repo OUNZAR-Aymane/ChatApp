208273463501132fcceb77032a5caeb8822d5fbe
YAOU Reda <yaoureda24@gmail.com>
2026-03-29 14:51:43 +0200

    add basic main and http verticles

--- src/main/java/chat/app/ChatApp/MainVerticle.java
+++ src/main/java/chat/app/ChatApp/MainVerticle.java
@@ -2,13 +2,24 @@ package chat.app.ChatApp;
 
 import chat.app.ChatApp.database.DatabaseVerticle;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 
 public class MainVerticle extends AbstractVerticle {
-
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    Future<String> dbVerticleDeployment = vertx.deployVerticle(new DatabaseVerticle());
+  public void start(Promise<Void> startPromise) {
+    Future<String> dbVerticleDeployment = vertx.deployVerticle(new
+      DatabaseVerticle()); //(1)
+    dbVerticleDeployment.compose(id ->
+        vertx.deployVerticle("io.vertx.guides.wiki.HttpServerVerticle", //(2)
+          new DeploymentOptions().setInstances(2))) //(3)
+      .onComplete(ar -> { //(4)
+        if (ar.succeeded()) {
+          startPromise.complete();
+        } else {
+          startPromise.fail(ar.cause());
+        }
+      });
   }
 }
--- src/main/java/chat/app/ChatApp/http/HttpServerVerticle.java
+++ src/main/java/chat/app/ChatApp/http/HttpServerVerticle.java
@@ -1,4 +1,122 @@
 package chat.app.ChatApp.http;
 
-public class HttpServerVerticle {
+import chat.app.ChatApp.database.DatabaseService;
+import chat.app.ChatApp.database.DatabaseVerticle;
+import io.vertx.core.*;
+
+import com.github.rjeschke.txtmark.Processor;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+import static chat.app.ChatApp.database.DatabaseVerticle.CONFIG_WIKIDB_QUEUE;
+
+public class HttpServerVerticle extends AbstractVerticle {
+  private FreeMarkerTemplateEngine templateEngine;
+  private DatabaseService dbService;
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    templateEngine = FreeMarkerTemplateEngine.create(vertx);
+    String wikiDbQueue = config().getString(CONFIG_WIKIDB_QUEUE, "wikidb.queue");
+
+    dbService = DatabaseService.createProxy(vertx, wikiDbQueue);
+    HttpServer server = vertx.createHttpServer();
+  }
+  private void indexHandler(RoutingContext context) {
+    dbService.fetchAllPages(reply -> {
+      if (reply.succeeded()) {
+        context.put("title", "Wiki home");
+        context.put("pages", reply.result().getList());
+        templateEngine.render(context.data(), "/templates/index.ftl", ar -> {
+          if (ar.succeeded()) {
+            context.response().putHeader("Content-Type", "text/html");
+            context.response().end(ar.result());
+          } else {
+            context.fail(ar.cause());
+          }
+        });
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+  private static final String EMPTY_PAGE_MARKDOWN = """
+  # A new page
+  Feel-free to write in Markdown!
+  """;
+  private void pageRenderingHandler(RoutingContext context) {
+    String requestedPage = context.request().getParam("page");
+    dbService.fetchPage(requestedPage, reply -> {
+      if (reply.succeeded()) {
+        JsonObject body = reply.result();
+        boolean found = body.getBoolean("found");
+        String rawContent = body.getString("rawContent", EMPTY_PAGE_MARKDOWN);
+        context.put("title", requestedPage);
+        context.put("id", body.getInteger("id", -1));
+        context.put("newPage", found ? "no" : "yes");
+        context.put("rawContent", rawContent);
+        context.put("content", Processor.process(rawContent));
+        context.put("timestamp", new Date().toString());
+        templateEngine.render(context.data(), "/templates/page.ftl", ar -> {
+          if (ar.succeeded()) {
+            context.response().putHeader("Content-Type", "text/html");
+            context.response().end(ar.result());
+          } else {
+            context.fail(ar.cause());
+          }
+        });
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+  private void pageCreateHandler(RoutingContext context) {
+    String pageName = context.request().getParam("name");
+    String location = "/wiki/" + pageName;
+    if (pageName == null || pageName.isEmpty()) {
+      location = "/";
+    }
+    context.response().setStatusCode(303);
+    context.response().putHeader("Location", location);
+    context.response().end();
+  }
+  private void pageUpdateHandler(RoutingContext context) {
+    String title = context.request().getParam("title");
+    Handler<AsyncResult<Void>> handler = reply -> {
+      if (reply.succeeded()) {
+        context.response().setStatusCode(303);
+        context.response().putHeader("Location", "/wiki/" + title);
+        context.response().end();
+      } else {
+        context.fail(reply.cause());
+      }
+    };
+    String markdown = context.request().getParam("markdown");
+    if ("yes".equals(context.request().getParam("newPage"))) {
+      dbService.createPage(title, markdown, handler);
+    } else {
+      dbService.savePage(Integer.valueOf(context.request().getParam("id")), markdown,
+        handler);
+    }
+  }
+  private void pageDeletionHandler(RoutingContext context) {
+    dbService.deletePage(Integer.valueOf(context.request().getParam("id")), reply -> {
+      if (reply.succeeded()) {
+        context.response().setStatusCode(303);
+        context.response().putHeader("Location", "/");
+        context.response().end();
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
 }
