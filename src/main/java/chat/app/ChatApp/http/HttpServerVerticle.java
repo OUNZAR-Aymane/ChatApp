4abb42d8fe3167472626b91a821ca5511519bfcb
YAOU Reda <yaoureda24@gmail.com>
2026-03-29 16:32:24 +0200

    add a functioning (I think) HTTP Verticle

--- src/main/java/chat/app/ChatApp/http/HttpServerVerticle.java
+++ src/main/java/chat/app/ChatApp/http/HttpServerVerticle.java
@@ -1,125 +1,94 @@
 package chat.app.ChatApp.http;
 
 import chat.app.ChatApp.database.DatabaseService;
-import chat.app.ChatApp.database.DatabaseVerticle;
-import io.vertx.core.*;
 
-import com.github.rjeschke.txtmark.Processor;
-import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.*;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
-
 import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
-
-import static chat.app.ChatApp.database.DatabaseVerticle.CONFIG_DB_QUEUE;
-
 public class HttpServerVerticle extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+
   private FreeMarkerTemplateEngine templateEngine;
   private DatabaseService dbService;
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    templateEngine = FreeMarkerTemplateEngine.create(vertx);
-    String DbQueue = config().getString(CONFIG_DB_QUEUE, "db.queue");
 
-    dbService = DatabaseService.createProxy(vertx, DbQueue);
+  @Override
+  public void start(Promise<Void> startPromise) {
+    templateEngine = FreeMarkerTemplateEngine.create(vertx);
+    String dbQueue = config().getString("db.queue", "db.queue");
+    dbService = DatabaseService.createProxy(vertx, dbQueue);
+
     HttpServer server = vertx.createHttpServer();
+    Router router = Router.router(vertx);
+    router.route().handler(BodyHandler.create());
+
+    router.get("/").handler(this::indexHandler);
+    router.post("/message").handler(this::messageCreationHandler);
+
+    server.requestHandler(router).listen(8080, ar -> {
+      if (ar.succeeded()) {
+        LOGGER.info("HTTP server started on port 8080");
+        startPromise.complete();
+      } else {
+        LOGGER.error("Failed to start HTTP server", ar.cause());
+        startPromise.fail(ar.cause());
+      }
+    });
   }
+  // handlers
   private void indexHandler(RoutingContext context) {
     dbService.fetchMessages(reply -> {
       if (reply.succeeded()) {
+
         context.put("title", "Chat home");
-        context.put("pages", reply.result().getList());
-        templateEngine.render(context.data(), "/templates/index.ftl", ar -> {
+        context.put("messages", reply.result().getList());
+
+        templateEngine.render(context.data(), "templates/index.ftl", ar -> {
           if (ar.succeeded()) {
-            context.response().putHeader("Content-Type", "text/html");
-            context.response().end(ar.result());
+            context.response()
+              .putHeader("Content-Type", "text/html")
+              .end(ar.result());
           } else {
             context.fail(ar.cause());
           }
         });
+
       } else {
+        LOGGER.error("Failed to fetch messages", reply.cause());
         context.fail(reply.cause());
       }
     });
   }
+
   private void messageCreationHandler(RoutingContext context) {
-    dbService.createMessage(reply -> {
-    });
-  }
-  // ça ça vient du TP
-  private static final String EMPTY_PAGE_MARKDOWN = """
-  # A new page
-  Feel-free to write in Markdown!
-  """;
-  private void pageRenderingHandler(RoutingContext context) {
-    String requestedPage = context.request().getParam("page");
-    dbService.fetchPage(requestedPage, reply -> {
-      if (reply.succeeded()) {
-        JsonObject body = reply.result();
-        boolean found = body.getBoolean("found");
-        String rawContent = body.getString("rawContent", EMPTY_PAGE_MARKDOWN);
-        context.put("title", requestedPage);
-        context.put("id", body.getInteger("id", -1));
-        context.put("newPage", found ? "no" : "yes");
-        context.put("rawContent", rawContent);
-        context.put("content", Processor.process(rawContent));
-        context.put("timestamp", new Date().toString());
-        templateEngine.render(context.data(), "/templates/page.ftl", ar -> {
-          if (ar.succeeded()) {
-            context.response().putHeader("Content-Type", "text/html");
-            context.response().end(ar.result());
-          } else {
-            context.fail(ar.cause());
-          }
-        });
-      } else {
-        context.fail(reply.cause());
-      }
-    });
-  }
-  private void pageCreateHandler(RoutingContext context) {
-    String pageName = context.request().getParam("name");
-    String location = "/chat/" + pageName;
-    if (pageName == null || pageName.isEmpty()) {
-      location = "/";
+
+    String sender = context.request().getParam("sender");
+    String message = context.request().getParam("message");
+
+    if (sender == null || sender.isEmpty() ||
+      message == null || message.isEmpty()) {
+      context.response()
+        .setStatusCode(400)
+        .end("Sender and message must not be empty");
+      return;
     }
-    context.response().setStatusCode(303);
-    context.response().putHeader("Location", location);
-    context.response().end();
-  }
-  private void pageUpdateHandler(RoutingContext context) {
-    String title = context.request().getParam("title");
-    Handler<AsyncResult<Void>> handler = reply -> {
+
+    dbService.createMessage(sender, message, reply -> {
       if (reply.succeeded()) {
-        context.response().setStatusCode(303);
-        context.response().putHeader("Location", "/chat/" + title);
-        context.response().end();
+        context.response()
+          .setStatusCode(303)
+          .putHeader("Location", "/")
+          .end();
+
       } else {
-        context.fail(reply.cause());
-      }
-    };
-    String markdown = context.request().getParam("markdown");
-    if ("yes".equals(context.request().getParam("newPage"))) {
-      dbService.createPage(title, markdown, handler);
-    } else {
-      dbService.savePage(Integer.valueOf(context.request().getParam("id")), markdown,
-        handler);
-    }
-  }
-  private void pageDeletionHandler(RoutingContext context) {
-    dbService.deletePage(Integer.valueOf(context.request().getParam("id")), reply -> {
-      if (reply.succeeded()) {
-        context.response().setStatusCode(303);
-        context.response().putHeader("Location", "/");
-        context.response().end();
-      } else {
+        LOGGER.error("Failed to create message", reply.cause());
         context.fail(reply.cause());
       }
     });
