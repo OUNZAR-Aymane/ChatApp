c22b1aa0319f33112e06ca1e07dce65f970fcfcf
YAOU Reda <yaoureda24@gmail.com>
2026-03-29 15:03:32 +0200

    fix httpVerticle path in MainVerticle

--- src/main/java/chat/app/ChatApp/MainVerticle.java
+++ src/main/java/chat/app/ChatApp/MainVerticle.java
@@ -12,7 +12,7 @@ public class MainVerticle extends AbstractVerticle {
     Future<String> dbVerticleDeployment = vertx.deployVerticle(new
       DatabaseVerticle()); //(1)
     dbVerticleDeployment.compose(id ->
-        vertx.deployVerticle("io.vertx.guides.HttpServerVerticle", //(2)
+        vertx.deployVerticle("chat.app.ChatApp.http.HttpServerVerticle", //(2)
           new DeploymentOptions().setInstances(2))) //(3)
       .onComplete(ar -> { //(4)
         if (ar.succeeded()) {
